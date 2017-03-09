a045d3194044939dc56f123fd17ea5ac1827e8f9
Ludovic Ollagnier <ludovic@tectec.fr>
2017-03-09 15:10:16 +0100

    WIP CollectionViewController

---
+++ Projet/Resto/Resto/Controllers/List/RestaurantCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  RestaurantCollectionViewCell.swift
+//  Resto
+//
+//  Created by Ludovic Ollagnier on 09/03/2017.
+//  Copyright © 2017 Ludovic Ollagnier. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ Projet/Resto/Resto/Controllers/List/RestaurantListCollectionViewController.swift
@@ -0,0 +1,90 @@
+//
+//  RestaurantListCollectionViewController.swift
+//  Resto
+//
+//  Created by Ludovic Ollagnier on 09/03/2017.
+//  Copyright © 2017 Ludovic Ollagnier. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantListCollectionViewController: UICollectionViewController {    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: UICollectionViewDataSource
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+
+        return 100
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "restoCell", for: indexPath)
+    
+        // Configure the cell
+    
+        if indexPath.row%2 == 0 {
+            cell.backgroundColor = UIColor.red
+        } else {
+            cell.backgroundColor = UIColor.yellow
+        }
+        
+        
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+    
+    }
+    */
+
+}
