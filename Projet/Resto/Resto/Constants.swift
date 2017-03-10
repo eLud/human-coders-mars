84d1e22a89d6320c545a4157bc69d672f268e3ae
Ludovic Ollagnier <ludovic@tectec.fr>
2017-03-10 11:07:31 +0100

    [feature] Added a constant file

---
+++ Projet/Resto/Resto/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  Resto
+//
+//  Created by Ludovic Ollagnier on 10/03/2017.
+//  Copyright © 2017 Ludovic Ollagnier. All rights reserved.
+//
+
+struct Constants {
+    
+    struct Notifications {
+        static let modelUpdated = "modelUpdatedNotificationName"
+        static let databaseCorruption = "dataBaseNotificationName"
+        static let somethingElse = "somethingElseNotificationName"
+    }
+    
+    struct UserDefaults {
+        static let maCléDeDEfaults = "gjgfghehe"
+    }
+}
