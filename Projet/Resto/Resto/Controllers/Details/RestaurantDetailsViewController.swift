c4b62a589ae726eec2c360d205a242fb9e464e6c
Ludovic Ollagnier <ludovic@tectec.fr>
2017-03-10 16:59:11 +0100

    [feature] Touch ID + URLSession

--- Projet/Resto/Resto/Controllers/Details/RestaurantDetailsViewController.swift
+++ Projet/Resto/Resto/Controllers/Details/RestaurantDetailsViewController.swift
@@ -7,10 +7,12 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class RestaurantDetailsViewController: UIViewController {
     
     @IBOutlet weak var restoAdressLabel: UILabel!
+    @IBOutlet weak var swiftImageView: UIImageView!
     
     var restaurant: Restaurant?
 
@@ -18,8 +20,28 @@ class RestaurantDetailsViewController: UIViewController {
         super.viewDidLoad()
 
         configureUI()
+        verifyUser()
+
     }
 
+    @IBAction func touchId(_ sender: Any) {
+        
+        let url = URL(string: "https://developer.apple.com/swift/images/swift-og.png")!
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            
+            if let image = data {
+                let swift = UIImage(data: image)
+                print(swift)
+                
+                DispatchQueue.main.async {
+                    self.swiftImageView.image = swift
+                }
+                
+            }
+        }
+        task.resume()
+    }
+    
     private func configureUI() {
         
         guard let resto = restaurant else {
@@ -27,6 +49,30 @@ class RestaurantDetailsViewController: UIViewController {
         }
         title = resto.name
         restoAdressLabel.text = resto.address
+    }
+    
+    private func verifyUser() {
+        let currentContext = LAContext()
+        var error: NSError?
+        
+        guard currentContext.canEvaluatePolicy(.deviceOwnerAuthentication, error: &error) else {
+            print(error)
+            return
+        }
+        
+        currentContext.evaluatePolicy(.deviceOwnerAuthentication, localizedReason: "Je veux savoir si tu es bien toi") { (authenticated, error) in
+            
+            guard error == nil else {
+                print("probleme")
+                return
+            }
+            
+            if authenticated {
+                print("super")
+            } else {
+                print("Tu n'es pas toi")
+            }
+        }
     }
     
 }
